f902a80583aea130b6e2744ab56124a470b15061
Velibor Mitić <velibor@castel.co.rs>
2019-02-27 15:02:31 +0100

    Improved indexOfMaxArrayElement method

--- src/Util.java
+++ src/Util.java
@@ -2,15 +2,6 @@ import java.util.Scanner;
 
 public class Util {
 
-    // Find the max element of array
-    /*public static int indexOfMaxArrayElement(int[] array) {
-        int indexOfMaxElement = 0;
-        for (int i = 0; i < array.length; i++) {
-            indexOfMaxElement = array[i] > array[indexOfMaxElement] ? i : indexOfMaxElement;
-        }
-        return indexOfMaxElement;
-    }*/
-
     public static int indexOfMaxArrayElement(int[] array) {
         int indexOfMaxElement = 0;
         int j = array.length - 1;
